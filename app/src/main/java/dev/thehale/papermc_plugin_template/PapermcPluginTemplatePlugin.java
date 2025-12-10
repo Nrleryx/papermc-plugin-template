d2f46ca6b3208b473046313e7d9a0922eb3a50b0
Ömer <nrleryx@gmail.com>
2025-12-10 22:54:01 +0300

    Update PapermcPluginTemplatePlugin.java

--- app/src/main/java/dev/thehale/papermc_plugin_template/PapermcPluginTemplatePlugin.java
+++ app/src/main/java/dev/thehale/papermc_plugin_template/PapermcPluginTemplatePlugin.java
@@ -1,8 +1,3 @@
-// Copyright (c) 2023 Joseph Hale
-// 
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package dev.thehale.papermc_plugin_template;
 
